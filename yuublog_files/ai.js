260ac82a0a8568ed3f6ca43554cc1e4e361fd27b
yuublog11 <130847222+yuublog11@users.noreply.github.com>
2023-04-15 23:08:05 +0900

    Add files via upload

---
+++ yuublog_files/ai.js
@@ -0,0 +1,27 @@
+// list追加
+
+$('.list-unstyled').prepend();
+
+
+// profileコメント変更
+
+$('.author').children("p").text();
+
+
+// よく読まれてる記事変更
+// text
+// 1
+$('.top').text();
+// 2
+$('.top1').text();
+// 3
+$('.top2').text();
+// 4
+$('.bottom1').text();
+// 5
+$('.bottom2').text();
+
+
+// profileのimg要素変更
+
+$('.img-circle').attr();
